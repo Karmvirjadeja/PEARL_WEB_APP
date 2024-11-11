b3f3a2d2889bba362cca22e6caab60abc3059de4
Knight <112848592+Karmvirjadeja@users.noreply.github.com>
2024-11-11 17:45:40 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src", // Alias if using @ notation for imports
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ["src/main.jsx"],
-    },
-  },
-});
+})
