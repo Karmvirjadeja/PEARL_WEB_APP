8f67f03ed493f2d118366086af47df2cdb2035aa
Knight <112848592+Karmvirjadeja@users.noreply.github.com>
2024-11-11 20:18:16 +0530

    Update Login.jsx

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate, Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import logo from "./logo.png";
 import safflogo from "./safflogo.png";
 import { toast, ToastContainer, Bounce } from "react-toastify";
@@ -9,10 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [otp, setOtp] = useState("");
   const [errors, setErrors] = useState({});
-  const [otpSent, setOtpSent] = useState(false);
-  const [showOtpError, setShowOtpError] = useState(false);
   const navigate = useNavigate();
 
   const toastOptions = {
@@ -45,51 +42,25 @@ const Login = () => {
 
     if (Object.keys(errors).length === 0) {
       try {
-        let response;
-        if (otpSent) {
-          response = await axios.post(
-            "https://pearl-web-app.onrender.com/api/auth/verify-otp",
-            { email, otp },
-            {
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-        } else {
-          response = await axios.post(
-            "https://pearl-web-app.onrender.com/api/auth/login",
-            { email, password },
-            {
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-
-          if (response.data.message === "OTP sent successfully") {
-            setOtpSent(true);
-            toast.success("OTP sent successfully", toastOptions);
-          } else {
-            const { token } = response.data;
-            localStorage.setItem("token", token);
-            toast.success("Login successful!", toastOptions);
-            setTimeout(() => {
-              navigate("/MainPage");
-            }, 3000); // Delay navigation by 3 seconds
+        const response = await axios.post(
+          "https://pearl-web-app.onrender.com/api/auth/login",
+          { email, password },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
           }
-        }
+        );
 
         const { token } = response.data;
         localStorage.setItem("token", token);
-        toast.success("OTP confirmed and login successful!", toastOptions);
+        toast.success("Login successful!", toastOptions);
         setTimeout(() => {
           navigate("/MainPage");
         }, 3000); // Delay navigation by 3 seconds
       } catch (error) {
         console.error("Error logging in:", error);
         toast.error("Invalid email or password. Please try again.", toastOptions);
-        setShowOtpError(true);
       }
     }
   };
@@ -129,7 +100,6 @@ const Login = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="w-full py-3 px-4 mb-1 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-800"
-              disabled={otpSent}
             />
             {errors.email && (
               <p className="w-full mb-4 text-sm text-red-600">{errors.email}</p>
@@ -141,7 +111,6 @@ const Login = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="w-full py-3 px-4 mb-1 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-800"
-              disabled={otpSent}
             />
             {errors.password && (
               <p className="w-full mb-4 text-sm text-red-600">
@@ -149,53 +118,26 @@ const Login = () => {
               </p>
             )}
 
-            {otpSent && (
-              <>
-                <input
-                  type="text"
-                  placeholder="Enter OTP"
-                  value={otp}
-                  onChange={(e) => setOtp(e.target.value)}
-                  className="w-full py-3 px-4 mb-1 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-800"
-                />
-                {showOtpError && (
-                  <p className="w-full mb-2 text-sm text-red-600">
-                    Incorrect OTP. Please try again.
-                  </p>
-                )}
-                <button
-                  onClick={handleLogin}
-                  className="w-full py-3 px-6 text-xl font-medium text-white bg-gray-800 hover:bg-gray-900 rounded"
-                >
-                  Confirm OTP
-                </button>
-              </>
-            )}
+            <button
+              onClick={handleLogin}
+              className="w-full py-3 px-6 mb-6 text-xl font-medium text-white bg-gray-800 hover:bg-gray-900 rounded"
+            >
+              Login
+            </button>
 
-            {!otpSent && (
-              <>
-                <button
-                  onClick={handleLogin}
-                  className="w-full py-3 px-6 mb-6 text-xl font-medium text-white bg-gray-800 hover:bg-gray-900 rounded"
-                >
-                  Login
-                </button>
-                <button
-                  onClick={handleForgotPassword}
-                  className="w-full py-3 px-6 text-lg text-orange-400 hover:text-orange-600 focus:outline-none"
-                >
-                  Forgot Password?
-                </button>
-              </>
-            )}
+            <button
+              onClick={handleForgotPassword}
+              className="w-full py-3 px-6 text-lg text-orange-400 hover:text-orange-600 focus:outline-none"
+            >
+              Forgot Password?
+            </button>
 
             <p className="mt-4">
               Don't have an account?{" "}
-              <Link to="/form"
+              <Link
+                to="/form"
                 className="text-orange-600 hover:text-orange-700 hover:underline"
-
               >
-
                 Create Account
               </Link>
             </p>
